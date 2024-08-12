982de3e7f205c314804689f99c6e344be5738c8f
Courtney Daly <courtneydaly1@gmail.com>
2024-08-12 19:04:25 -0400

    added routes and navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,40 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { BrowserRouter } from "react-router-dom";
+import RouteList from "./Routelist";
+import NavBar from "./NavBar"
 import './App.css';
 
+
 function App() {
+  const [dogs, setDogs] = useState({
+    data:null,
+    isLoading: true
+  });
+
+  useEffect(()=> {
+    async function loadDogs(){
+      const res= await axios.get("http://localhost:5000/dogs")
+      setDogs({
+        data: res.data,
+        isLoading: false
+      })
+    }
+    loadDogs()
+  }, [])
+  if(dogs.isLoading){
+    return <h2>Loading...</h2>
+  }
   return (
-    <div className="App">
-      <h1>Good luck!</h1>
-    </div>
+   <div>
+    <h1>Welcome</h1>
+    <BrowserRouter>
+      <NavBar dogs={dogs.data} />
+      <div>
+        <RouteList dogs={dogs.data} />
+      </div>
+    </BrowserRouter>
+   </div>
   );
 }
 
