8777d7cabd25e64b2198350df87f555d428a387e
Courtney Daly <courtneydaly1@gmail.com>
2024-08-12 18:24:11 -0400

    initial commit. start DogDetails.js

---
+++ src/DogDetails.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { Link, Navigate } from "react-router-dom";
+
+
+
+function DogDetails({ dog }){
+    if (!dog) return <Navigate to="/dogs" />
+    return (
+        <div className='DogDetail-row'>
+            <div>
+                <img src="`/${dog.src}.jpg`" alt={dog.name} />
+                <h1>{dog.name}</h1>
+                <h2>{dog.age} years old.</h2>
+                <ul>
+                    {dog.facts.map((fact, i)=>(
+                        <li key = {i}>
+                            {fact}
+                        </li>
+                    ))}
+                </ul>
+                <Link to="/dogs">Go Back</Link>
+
+            </div>
+
+        </div>
+    );
+}
+
+export default DogDetails;
